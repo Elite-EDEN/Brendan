2313a4d5a8c31394bee9fe0aa4ab35fe09f6e7b6
Dave Alvarado <d.alvarado@markspp.com>
2024-04-15 13:25:01 -0500

    Cleaned up code, initial instructions in README

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Brendan
+
+Brendan is a Discord bot used by the Elite Deep-Space Explorers Network.  It is written in C# 
+using .NET 8 and DSharpPlus.  Brendan is named for Brendan of Clonfert, the patron saint of
+sailors and travelers.
+
+## Contribution
+
+To contribute to Brendan you can use any tool capable of editing C# code.  Recommended tools 
+are Visual Studio, Visual Studio Code, and Rider.  Brendan is designed to be hosted in a 
+container.  The production copy of Brendan is hosted on Azure in a Container Instance.  Development
+work can be done using Docker Desktop.
+
+Brendan expects an environment variable called DISCORD_BOT_TOKEN to exist and contain a valid Discord
+bot token.  It is recommended that you create your own Discord bot application to obtain a token to use
+in your own environment.  The production version of Brandon has a token stored in a GitHub secret that
+gets passed to the production container via the CI/CD pipeline.
+
+For a tutorial on how Brendan was initially set up, see https://swimburger.net/blog/azure/how-to-create-a-discord-bot-using-the-dotnet-worker-template-and-host-it-on-azure-container-instances.
+
+## License
+
+Brendan is licensed by the Elite Deep-Space Explorers Network with the MIT license.
--- Worker.cs
+++ Worker.cs
@@ -1,52 +1,40 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using DSharpPlus;
 using DSharpPlus.EventArgs;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace EDEN.Breandan
 {
-    public class Worker : BackgroundService
-    {
-        private ILogger<Worker> logger;
-        private IConfiguration configuration;
-        private DiscordClient discordClient;
+    public class Worker(ILogger<Worker> logger, IConfiguration configuration) : BackgroundService
+	{
+		private readonly IConfiguration _configuration = configuration;
+        private DiscordClient? _discordClient;
 
-        public Worker(ILogger<Worker> logger, IConfiguration configuration)
-        {
-            this.logger = logger;
-            this.configuration = configuration;
-        }
-
-        public override async Task StartAsync(CancellationToken cancellationToken)
+		public override async Task StartAsync(CancellationToken cancellationToken)
         {
             logger.LogInformation("Starting discord bot");
 
-            string discordBotToken = configuration["DiscordBotToken"];
-            discordClient = new DiscordClient(new DiscordConfiguration()
+            var discordBotToken = Environment.GetEnvironmentVariable("DISCORD_BOT_TOKEN");
+            _discordClient = new DiscordClient(new DiscordConfiguration()
             {
                 Token = discordBotToken,
                 TokenType = TokenType.Bot,
                 Intents = DiscordIntents.AllUnprivileged
             });
 
-            discordClient.MessageCreated += OnMessageCreated;
-            await discordClient.ConnectAsync();
+            _discordClient.MessageCreated += OnMessageCreated;
+            await _discordClient.ConnectAsync();
         }
 
         protected override Task ExecuteAsync(CancellationToken stoppingToken) =>  Task.CompletedTask;
 
         public override async Task StopAsync(CancellationToken cancellationToken)
         {
-            discordClient.MessageCreated -= OnMessageCreated;
-            await discordClient.DisconnectAsync();
-            discordClient.Dispose();
-            logger.LogInformation("Discord bot stopped");
+			if (_discordClient != null) {
+				_discordClient.MessageCreated -= OnMessageCreated;
+				await _discordClient.DisconnectAsync();
+				_discordClient.Dispose();
+			}
+
+			logger.LogInformation("Discord bot stopped");
         }
 
         private async Task OnMessageCreated(DiscordClient client, MessageCreateEventArgs e)
