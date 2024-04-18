1aebb16422c3bef2fca0d45fb76e045adf7a157b
Dave Alvarado <d.alvarado@markspp.com>
2024-04-18 16:51:48 -0500

    Trying the commands without the static attribute

--- Commands/MyFirstModule.cs
+++ Commands/MyFirstModule.cs
@@ -9,13 +9,13 @@ namespace Brendan.Commands
 	public class MyFirstModule : BaseCommandModule
 	{
 		[Command("greet")]
-		public static async Task GreetCommand(CommandContext ctx, DiscordMember member)
+		public async Task GreetCommand(CommandContext ctx, DiscordMember member)
 		{
 			await ctx.RespondAsync($"Greetings, {member.Mention}! Enjoy the mention!");
 		}
 
 		[Command("random")]
-		public static async Task RandomCommand(CommandContext ctx, int min, int max)
+		public async Task RandomCommand(CommandContext ctx, int min, int max)
 		{
 			var random = new Random();
 			await ctx.RespondAsync($"Your number is: {random.Next(min, max)}");
