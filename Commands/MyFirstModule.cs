ac75e2433b6479b8619d8bd84f923c4da703b14a
Dave Alvarado <d.alvarado@markspp.com>
2024-04-18 16:46:50 -0500

    Added Random command and  updated Greet to use the DiscordMember type

--- Commands/MyFirstModule.cs
+++ Commands/MyFirstModule.cs
@@ -1,14 +1,24 @@
-﻿using DSharpPlus.CommandsNext;
+﻿// ReSharper disable UnusedMember.Global
+
+using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 
 namespace Brendan.Commands
 {
 	public class MyFirstModule : BaseCommandModule
 	{
 		[Command("greet")]
-		public async Task GreetCommand(CommandContext ctx, string name)
+		public static async Task GreetCommand(CommandContext ctx, DiscordMember member)
 		{
-			await ctx.RespondAsync($"Greetings, {name}! You're pretty neat!");
+			await ctx.RespondAsync($"Greetings, {member.Mention}! Enjoy the mention!");
+		}
+
+		[Command("random")]
+		public static async Task RandomCommand(CommandContext ctx, int min, int max)
+		{
+			var random = new Random();
+			await ctx.RespondAsync($"Your number is: {random.Next(min, max)}");
 		}
 	}
 }
